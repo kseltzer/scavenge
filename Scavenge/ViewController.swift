878c7b97795a3dbf00b4a256b395ff1d565503de
Kimberly Seltzer <kimseltzer@kimberlys-macbook-pro.local>
2016-07-10 11:52:03 -0700

    Initial Commit

---
+++ Scavenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scavenge
+//
+//  Created by Kimberly Seltzer on 7/10/16.
+//  Copyright © 2016 Kim Seltzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
