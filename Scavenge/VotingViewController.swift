a85cedf710a420b5ef4079e02e7cbd66638f47da
Kimberly Seltzer <kimseltzer@Kimberlys-MBP.home>
2016-09-09 14:37:52 -0700

    added submit button to voting screen

--- Scavenge/VotingViewController.swift
+++ Scavenge/VotingViewController.swift
@@ -17,6 +17,7 @@ let cellWidth = screenSize.width - (leadingSpace + trailingSpace)
 class VotingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, VotingCellDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var allVotesSubmitted : Bool = false
     var scrollViewOffsets : [CGFloat] = []
     
     override func viewDidLoad() {
@@ -24,11 +25,6 @@ class VotingViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let bottomSpace : CGFloat = 8
-        let pageControlHeight : CGFloat = 37
-        let imageViewHeight = cellWidth * IMAGE_RATIO
-        tableView.rowHeight = imageViewHeight + bottomSpace + pageControlHeight
         tableView.scrollsToTop = true
         
         navigationItem.backBarButtonItem = customBackBarItem()
@@ -39,7 +35,7 @@ class VotingViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     // MARK: - UITableViewDelegate
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return NUM_GAME_QUESTIONS
+        return NUM_GAME_QUESTIONS + 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,11 +52,47 @@ class VotingViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return sectionHeaderView
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section >= NUM_GAME_QUESTIONS {
+            let buttonHeight : CGFloat = 55
+            let topSpace : CGFloat = 8, bottomSpace : CGFloat = 32
+            return buttonHeight + topSpace + bottomSpace
+        }
+        
+        let bottomSpace : CGFloat = 8
+        let pageControlHeight : CGFloat = 37
+        let imageViewHeight = cellWidth * IMAGE_RATIO
+        let rowHeight = imageViewHeight + bottomSpace + pageControlHeight
+        return rowHeight
+    }
+    
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section >= NUM_GAME_QUESTIONS {
+            return 0
+        }
+        
         return 30
     }
     
+    func configureSubmitCell(cell: SubmitCell) -> SubmitCell {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToResultsViewController))
+        cell.submitButton.addGestureRecognizer(tapGesture)
+        if (self.allVotesSubmitted) {
+            cell.submitButton.enabled = true
+                cell.submitButton.alpha = 1.0
+            } else {
+            cell.submitButton.enabled = true // TODO: replace true with false
+                cell.submitButton.alpha = 0.6
+            }
+        return cell
+        }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section >= NUM_GAME_QUESTIONS {
+            let cell = tableView.dequeueReusableCellWithIdentifier("submitCell") as! SubmitCell
+            return configureSubmitCell(cell)
+        }
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("photosForTopicCell") as! VotingCell
         cell.delegate = self
         cell.index = indexPath.section
@@ -87,9 +119,11 @@ class VotingViewController: UIViewController, UITableViewDelegate, UITableViewDa
         scrollViewOffsets[index] = scrollPosition
     }
 
-    /*
     // MARK: - Navigation
-
+    func goToResultsViewController() {
+        self.performSegueWithIdentifier("showGameResults", sender: self)
+    }
+    /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
